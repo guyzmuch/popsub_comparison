41fde411aba7e656e1a15c910db05a4e9a5ed171
guyzmuch <guyzmuch@gmail.com>
2018-09-06 21:03:23 +0200

    work in progress

--- socketio_example.js
+++ socketio_example.js
@@ -4,24 +4,65 @@ var app = require('express')(),
     ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
     fs = require('fs');
 
+
+var rooms = {
+
+}
+
+var sockets = {
+
+}
 // Chargement de la page index.html
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index_socket.html');
 });
+app.get('/room1', function (req, res) {
+  res.sendfile(__dirname + '/index_socket_room1.html');
+});
+app.get('/room2', function (req, res) {
+  res.sendfile(__dirname + '/index_socket_room2.html');
+});
+app.get('/room3', function (req, res) {
+  res.sendfile(__dirname + '/index_socket_room3.html');
+});
 
 io.sockets.on('connection', function (socket, pseudo) {
     // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
-    socket.on('nouveau_client', function(pseudo) {
-        pseudo = ent.encode(pseudo);
-        socket.pseudo = pseudo;
-        socket.broadcast.emit('nouveau_client', pseudo);
+    socket.on('nouveau_client', function(data) {
+        pseudo = ent.encode(data.pseudo);
+        socket.pseudo = data.pseudo
+
+        if (!rooms[data.room]){
+          rooms[data.room] = {
+            users: [],
+            message: [],
+          }
+        }
+        rooms[data.room].users.push(socket.pseudo)
+        sockets[socket.pseudo] = data.room
+
+
+
+        socket.on('disconnect', function () {
+          delete sockets[socket.pseudo]
+          for (let i=0, iLen = rooms[data.room].users.length; i < iLen; i++){
+            if (rooms[data.room].users[i] === socket.pseudo){
+              rooms[data.room].users.splice(i, 1)
+            }
+          }
+          socket.broadcast.emit('leave_client_room_'+data.room, data.pseudo);
+          socket.broadcast.emit('users_room_'+data.room, rooms[data.room].users);
+        });
+
+        socket.on('message_room_'+data.room, function (message) {
+            message = ent.encode(message);
+            socket.broadcast.emit('message_room_'+data.room, {pseudo: socket.pseudo, message: message});
+        });
+
+        socket.broadcast.emit('nouveau_client_room_'+data.room, data.pseudo);
+        socket.broadcast.emit('users_room_'+data.room, rooms[data.room].users);
     });
 
-    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
-    socket.on('message', function (message) {
-        message = ent.encode(message);
-        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
-    });
 });
 
 server.listen(8080);
